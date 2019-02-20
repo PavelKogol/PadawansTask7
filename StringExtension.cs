821c2e6459a7895335d286d480ba2497869b2e59
PavelKogol <pavelkogol19@gmail.com>
2019-02-20 13:55:17 +0200

    Task complited

--- StringExtension.cs
+++ StringExtension.cs
@@ -4,9 +4,18 @@ namespace PadawansTask7
 {
     public class StringExtension
     {
-        public static void OrderStringsByLength(string[] array)
+        public static string [] OrderStringsByLength(string[] array)
         {
-            // put your code here
+            for (int i = 0; i < array.Length - 1; i++) {
+                for (int j = i + 1; j < array.Length; j++) {
+                    if (array[i].Length > array[j].Length) {
+                        string temp = array[j];
+                        array[j] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
+            return array;
             throw new NotImplementedException();
         }
     }
